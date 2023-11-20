2dba3dd8edfbf76fb273d34fb77ed528d43fadad
Devopscasting <devopcasting@gmail.com>
2023-11-20 12:48:29 +0530

    sleep 2

--- document_db/filter_in_progress.py
+++ document_db/filter_in_progress.py
@@ -43,7 +43,7 @@ class FilterInProgress:
                 document_sub_path = ""
                 document_path = ""
                 document_path_list = []
-            sleep(10)
+            sleep(2)
 
     
     # Insert new IN_PROGRESS document information in ocrrworkspace-ocrr.

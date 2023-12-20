25594abbcc581d7b5bb6d6acdd38097bbf949fed
Devopscasting <devopcasting@gmail.com>
2023-12-20 17:48:20 +0530

    passport ocr

--- passport/document_info.py
+++ passport/document_info.py
@@ -18,8 +18,11 @@ class PassportDocumentInfo:
 
         # Get the coordinates of all the extracted text
         self.coordinates = TextCoordinates(document_path, doc_type="passport").generate_text_coordinates()
+        print(self.coordinates)
         # Get the texts from document
-        self.text = pytesseract.image_to_string(document_path)
+        tesseract_config = r'-l eng --oem 3 --psm 11'
+        self.text = pytesseract.image_to_string(document_path, config=tesseract_config)
+
     
     def __extract_passport_number(self) -> list:
         matching_line_index_top = None
@@ -51,7 +54,7 @@ class PassportDocumentInfo:
         if matching_line_index_bottom is None:
             return result
         
-        # find the top Passport coordinates
+        # find the bottom Passport coordinates
         for i in range(matching_line_index_bottom + 1, len(self.coordinates)):
             text = self.coordinates[i][4]
             if matching_passport_text in text:
@@ -65,8 +68,165 @@ class PassportDocumentInfo:
         result.append(matching_passport_number_coords_bottom)
         return result
 
+    def __extract_dates(self) -> list:
+        # date pattern DD/MM/YYY
+        date_pattern = r'\d{2}/\d{2}/\d{4}'
+        date_coordinates = []
+        result = []
 
+        for i, (x1,y1,x2,y2,text) in enumerate(self.coordinates):
+            date_match = re.search(date_pattern, text)
+            if date_match:
+                date_coordinates.append([x1, y1, x2, y2])
 
+        if not date_coordinates:
+            return result
+        
+        # Get the first 6 chars
+        for i in date_coordinates:
+            width = i[2] - i[0]
+            result.append([i[0], i[1], i[0] + int(0.54 * width), i[3]])
+        return result
+
+    def __extract_gender(self) -> list:
+        gender_text = ['M', 'F']
+        result = []
+
+        for i, (x1,y1,x2,y2,text) in enumerate(self.coordinates):
+            if text in gender_text:
+                result = [x1, y1, x2, y2]
+                break
+        return result
+
+    def __extract_surname(self):
+        matching_text = "Surname"
+        surname = []
+        result = []
+        # split the text into lines
+        lines = [i for i in self.text.splitlines() if len(i) != 0]
+        
+        # find the line that matches search text
+        matching_line_index = self.__find_matching_line_index(lines, matching_text)
+        if matching_line_index == 0:
+            return result
+        print(lines)
+        # get the next line in the text
+        next_line_list = []
+        for line in lines[matching_line_index + 2 :]:
+            if line.lower() in 'faa ora arr /given names':
+                break
+            else:
+                next_line_list.append(line)
+
+        if not next_line_list:
+            result
+
+        for i in next_line_list:
+            for k, (x1, y1, x2, y2, text) in enumerate(self.coordinates):
+                if i == text:
+                    surname.append([x1, y1, x2, y2])
+        
+        for i in surname:
+            width = i[2] - i[0]
+            result.append([i[0], i[1], i[0] + int(0.40 * width), i[3]])
+
+        return result
+
+    def __extract_given_name(self):
+        matching_text = 'Names'
+        given_name = []
+        result = []
+
+        # split the text into lines
+        lines = [i for i in self.text.splitlines() if len(i) != 0]
+
+        # find the line that matches search text
+        matching_line_index = self.__find_matching_line_index(lines, matching_text)
+        if matching_line_index == 0:
+            return result
+
+        # get the next line in the text
+        next_line_list = []
+        for line in lines[matching_line_index + 1 :]:
+            if line.lower() in 'fier /sex':
+                break
+            else:
+                next_line_list.append(line)
+
+        if not next_line_list:
+            result
+
+        for i in next_line_list:
+            for k, (x1, y1, x2, y2, text) in enumerate(self.coordinates):
+                if i == text:
+                    given_name.append([x1, y1, x2, y2])
+        
+        for i in given_name:
+            width = i[2] - i[0]
+            result.append([i[0], i[1], i[0] + int(0.40 * width), i[3]])
+
+        return result
+    
+    def __extract_address(self):
+        matching_text = 'Address'
+        result = []
+
+        # split the text into lines
+        lines = [i for i in self.text.splitlines() if len(i) != 0]
+
+        # find the line that matches search text
+        matching_line_index = self.__find_matching_line_index(lines, matching_text)
+        if matching_line_index == 0:
+            return result
+
+        # get the next line in the text
+        next_line_list = []
+        for line in lines[matching_line_index + 1 :]:
+            if len(line) == 6 and line.isdigit():
+                break
+            else:
+                next_line_list.append(line)
+
+        if not next_line_list:
+            result
+
+        for i in next_line_list:
+            for k, (x1, y1, x2, y2, text) in enumerate(self.coordinates):
+                if i == text:
+                    result.append([x1, y1, x2, y2])
+        return result
+
+    def __find_matching_line_index(self, lines: list, matching_text: str ) -> int:
+        # find the line that matches search text
+        for i,line in enumerate(lines):
+            if matching_text in line:
+                return i
+        return 0
+    
+    def __extract_ind_name(self):
+        matching_text = 'IND'
+        result = []
+        for i,(x1, y1, x2, y2, text) in enumerate(self.coordinates):
+            if "IND" in text and '<' in text:
+                result.append([x1, y1, x2, y2])
+                break
+        return result
+    
+    def __extract_pin_number(self):
+        pin_coords = []
+        result = []
+        for i,(x1, y1, x2, y2, text) in enumerate(self.coordinates):
+            if len(text) == 6 and text.isdigit():
+                pin_coords.append([x1, y1, x2, y2])
+                break
+        
+        for i in pin_coords:
+            width = i[2] - i[0]
+            result.append([i[0], i[1], i[0] + int(0.10 * width), i[3]])
+        
+        return result
+
+        
     # func: collect pancard information
     def collect_passport_info(self) -> list:
         passport_doc_info_list = []
@@ -78,5 +238,69 @@ class PassportDocumentInfo:
             UpdateDocumentStatus(self.original_document_path, "REJECTED", "ERRPASS1").update_status()
             return passport_doc_info_list
         passport_doc_info_list.extend(passport_number)
+
+        # Collect: Dates
+        passport_dates = self.__extract_dates()
+        if not passport_dates:
+            self.logger.error(f"| Document Rejected with error ERRPASS2: {self.original_document_path}")
+            UpdateDocumentStatus(self.original_document_path, "REJECTED", "ERRPASS2").update_status()
+            passport_doc_info_list = []
+            return passport_doc_info_list
+        passport_doc_info_list.extend(passport_dates)
+
+        # Collect: Gender
+        passport_gender = self.__extract_gender()
+        if not passport_gender:
+            self.logger.error(f"| Document Rejected with error ERRPASS3: {self.original_document_path}")
+            UpdateDocumentStatus(self.original_document_path, "REJECTED", "ERRPASS3").update_status()
+            passport_doc_info_list = []
+            return passport_doc_info_list
+        passport_doc_info_list.append(passport_gender)
+
+        # Collect: Surname
+        passport_surname = self.__extract_surname()
+        if not passport_surname:
+            self.logger.error(f"| Document Rejected with error ERRPASS4: {self.original_document_path}")
+            UpdateDocumentStatus(self.original_document_path, "REJECTED", "ERRPASS4").update_status()
+            passport_doc_info_list = []
+            return passport_doc_info_list
+        passport_doc_info_list.extend(passport_surname)
+
+        # Collect: Given Name
+        passport_given_name = self.__extract_given_name()
+        if not passport_given_name:
+            self.logger.error(f"| Document Rejected with error ERRPASS5: {self.original_document_path}")
+            UpdateDocumentStatus(self.original_document_path, "REJECTED", "ERRPASS5").update_status()
+            passport_doc_info_list = []
+            return passport_doc_info_list
+        passport_doc_info_list.extend(passport_given_name)
+
+        # Collect: IND Name
+        passport_ind_name = self.__extract_ind_name()
+        if not passport_ind_name:
+            self.logger.error(f"| Document Rejected with error ERRPASS6: {self.original_document_path}")
+            UpdateDocumentStatus(self.original_document_path, "REJECTED", "ERRPASS6").update_status()
+            passport_doc_info_list = []
+            return passport_doc_info_list
+        passport_doc_info_list.extend(passport_ind_name)
+
+        # Collect: Address
+        # passport_address = self.__extract_address()
+        # if not passport_address:
+        #     self.logger.error(f"| Document Rejected with error ERRPASS7: {self.original_document_path}")
+        #     UpdateDocumentStatus(self.original_document_path, "REJECTED", "ERRPASS7").update_status()
+        #     passport_doc_info_list = []
+        #     return passport_doc_info_list
+        # passport_doc_info_list.extend(passport_address)
+
+        # Collect: Pin Number
+        passport_pin_number = self.__extract_pin_number()
+        if not passport_pin_number:
+            self.logger.error(f"| Document Rejected with error ERRPASS6: {self.original_document_path}")
+            UpdateDocumentStatus(self.original_document_path, "REJECTED", "ERRPASS6").update_status()
+            passport_doc_info_list = []
+            return passport_doc_info_list
+        passport_doc_info_list.extend(passport_pin_number)
+
         return passport_doc_info_list
 

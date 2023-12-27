09cae7fafc414f377a066f8666b62bec03af71d9
Devopscasting <devopcasting@gmail.com>
2023-12-27 17:35:39 +0530

    adding frame id in xml

--- helpers/xmldata.py
+++ helpers/xmldata.py
@@ -7,22 +7,36 @@ class WriteXML:
         self.xml_path = redacted_path
         self.xml_file_name = original_document_name
         self.content = ocr_obj_info
-        # Get Document ID
-        doc_id_num = re.split('_', self.xml_file_name)[0]
-        self.doc_id = doc_id_num[:-1]
+       
+         
 
     def writexml(self) -> bool:
+
         # Set XML file path
         xml_file_path = os.path.join(self.xml_path, self.xml_file_name.split('.')[0]+'.xml')
         if os.path.exists(xml_file_path):
             os.remove(xml_file_path)
 
         # Prepare data
+        # Check if Frame-ID is available
+        pattern = "^[0-9]+F[0-9a-fA-Z_-]+"
+        frame_str = self.xml_file_name.split('.')[0].split('-')[0]
+        matched_str = re.match(pattern, self.xml_file_name)
+        frame_id = 0
+        if matched_str:
+            frame_id = int(frame_str.split('F')[0]) - 1
+            # Get Document ID
+            doc_id = re.split('_', self.xml_file_name)[0].split('-')[1][:-1]
+        else:
+            # Get Document ID
+            doc_id_num = re.split('_', self.xml_file_name)[0]
+            doc_id = doc_id_num[:-1]
+
         data = []
         count_index = 1
         for i in self.content:
             x1, y1, x2, y2 = i
-            data.append(f'0,0,0,,,,0,0,0,0,0,0,,vv,CVDPS,vv,0,{self.doc_id},0,{count_index},{x1},{y1},{x2},{y2},0,0,')
+            data.append(f'0,0,0,,,,0,0,0,0,0,0,,vv,CVDPS,vv,{frame_id},{doc_id},0,{count_index},{x1},{y1},{x2},{y2},0,0,')
             x1, y1, x2, y2 = [0, 0, 0, 0]
             count_index = count_index + 1
         # Create the root element    
@@ -45,5 +59,4 @@ class WriteXML:
 
         # Write the XML to a file or print it
         tree.write(xml_file_path , encoding="utf-8", xml_declaration=True)
-
         return True
